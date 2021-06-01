32e214870371c98a69412eb6f8091d0e64de9ae3
paulsimonsalcedo <85142484+paulsimonsalcedo@users.noreply.github.com>
2021-06-01 22:07:09 +0800

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,13 @@
+	
+
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back_to_top').fadeIn();
+        } else {
+            $('#back_to_top').fadeOut();
+        }
+    });
+
+	
+
+	
